9231791d9d9d8df0f550afa560d7fa6e4cd8fded
Wibulization <nhmkhoi123@gmail.com>
2024-04-26 14:48:28 +0700

    firebase seen (1/2) + view comic

--- app/src/main/java/com/example/Project_android_2/activities/SeenComic.java
+++ app/src/main/java/com/example/Project_android_2/activities/SeenComic.java
@@ -1,17 +1,34 @@
 package com.example.Project_android_2.activities;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.Project_android_2.R;
+import com.example.Project_android_2.activities.RC_recyclerView.RCAdapter_Chap;
+import com.example.Project_android_2.activities.RC_recyclerView.RCModel_Chap;
+import com.example.Project_android_2.models.chapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SeenComic extends AppCompatActivity {
     private View viewLogo;
@@ -25,7 +42,14 @@ public class SeenComic extends AppCompatActivity {
     private Button btnConfirm;
     private TextView txtview_Title;
     private TextView txtview_Author;
-    private TextView txtview_Chapter;
+    private RecyclerView rc_Chapter;
+    private RCAdapter_Chap rcAdapter;
+
+    ArrayList<RCModel_Chap> modelArrayList;
+
+    private StorageReference storageReference;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +66,88 @@ public class SeenComic extends AppCompatActivity {
         btnConfirm = findViewById(R.id.confirm_button);
         txtview_Author = findViewById(R.id.author);
         txtview_Title = findViewById(R.id.textViewTitle);
-        txtview_Chapter = findViewById(R.id.chap1);
 
-        txtview_Chapter.setOnClickListener(new View.OnClickListener() {
+        storageReference = FirebaseStorage.getInstance().getReference("CHAPTER");
+        databaseReference = FirebaseDatabase.getInstance().getReference("chapter");
+
+        createListChap();
+        /*
+         * txtview_Chapter.setOnClickListener(new View.OnClickListener() {
+         * 
+         * @Override
+         * public void onClick(View view) {
+         * Drawable drawable = getResources().getDrawable(R.drawable.retangle_red);
+         * txtview_Chapter.setBackground(drawable);
+         * Intent intent = new Intent(SeenComic.this, ViewComic.class);
+         * startActivity(intent);
+         * }
+         * });
+         */
+        Log.d("test", "test" + rc_Chapter);
+        imageViewRefund.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Drawable drawable = getResources().getDrawable(R.drawable.retangle_red);
-                txtview_Chapter.setBackground(drawable);
-                Intent intent = new Intent(SeenComic.this, ViewComic.class);
+                Intent intent = new Intent(SeenComic.this, Home.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    public void createListChap() {
+        rc_Chapter = findViewById(R.id.rc_chapter);
+        rc_Chapter.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rc_Chapter.setHasFixedSize(true);
+        modelArrayList = new ArrayList<>();
+        getIndexChap();
+    }
+
+    public void getIndexChap() {
+        // Intent intent1 = getIntent();
+        // String idcomic = intent1.getStringExtra("ID");
+        Query query = databaseReference.orderByChild("ID_COMIC").equalTo("a5366deb-7b91-473a-a9c1-05107154cfed");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot chapterSnapshot : snapshot.getChildren()) {
+                        String id = chapterSnapshot.getKey();
+                        String content = chapterSnapshot.child("CONTENT").getValue(String.class);
+                        String title = chapterSnapshot.child("TITLE").getValue(String.class);
+                        Long chapterIndex = chapterSnapshot.child("CHAPTER_INDEX").getValue(Long.class);
+                        String index = chapterIndex + "";
+                        chapter Chapter = new chapter(id, title, index, content,
+                                "a5366deb-7b91-473a-a9c1-05107154cfed");
+                        RCModel_Chap rc = new RCModel_Chap(Chapter);
+                        modelArrayList.add(rc);
+                        Log.d("MyApp", "Giá trị số nguyên: " + content);
+                    }
+                }
+
+                Collections.sort(modelArrayList);
+                Integer size = modelArrayList.size();
+                rcAdapter = new RCAdapter_Chap(SeenComic.this, modelArrayList);
+                rc_Chapter.setAdapter(rcAdapter);
+                rcAdapter.notifyDataSetChanged();
+                rcAdapter.setOnItemClickListener(new RCAdapter_Chap.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(chapter position) {
+                        Intent intent = new Intent(SeenComic.this, ViewComic.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("ID", position.getId());
+                        bundle.putString("CONTENT", position.getContent());
+                        bundle.putString("CHAPTER_INDEX", position.getIndex());
+                        bundle.putString("ID_COMIC", position.getId_comic());
+                        bundle.putString("TITLE", position.getTitle());
+                        bundle.putInt("SIZE", size);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
